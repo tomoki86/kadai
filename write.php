da679f1278bb2004fb72677403f583546990b192
tomoki sato <tomoki.akinori@gmail.com>
2019-01-05 21:06:50 +0900

    レイアウト変更

--- write.php
+++ write.php
@@ -1,49 +1,56 @@
 <?php
-// require('dbconnect');
-
-date_default_timezone_set('japan');
-$time = intval(date('H'));
+// $sql='INSERT INTO`diary`(`title`,`contents`,`created`)VALUES(?,?,NOW());';
+// $stmt=$dbh->prepare($sql);
+// $stml=execute();
 
 
 ?>
-
+<!DOCTYPE html>
+<html lang="ja">
 <?php include('layouts/header.php'); ?>
 <body>
   <div class="container-fluid">
-
     <?php include('navbar.php'); ?>
-
     <div class="row">
-      <div class="col-xs-12">
-
-        <div class="col-xs-3">
+      <div class="col-lg-12">
+         <div class="col-xs-3">
           <h4>
-          <?php if (6 <= $time && $time <= 11) {
-          echo 'おはようございます';
-          }elseif (11 < $time && $time < 18) {
-          echo 'こんにちわ、ゲストさん';
-          }else {
-          echo 'こんばんわ、ゲストさん';
-          }
-          ?>
+            <p>log</p>
           </h4><br>
           <a class="tablinks" onclick="openCity(event, 'New')" id="defaultOpen"><?php
             // date()で日時を出力
             echo date( "Y/n" ) ;
             ?>月の日記</a><hr>
 
-          <a class="tablinks" onclick="openCity(event, 'Pre')"><?php
+          <a class="tablinks" onclick="openCity(event, '1')"><?php
+            // date()で日時を出力
+            echo date( "Y/n" ) ;
+            ?>月の日記</a><hr>
+
+          <a class="tablinks" onclick="openCity(event, '2')"><?php
             echo date( "Y/n" , strtotime(date('Y/n/1') . '-1 month'));
             // date()で日時を出力
             ;?>月の日記</a><hr>
 
-          <a class="tablinks" onclick="openCity(event, '2mon')"><?php
-            echo date("Y/n", strtotime(date('Y/n/1') . '-2 month'));
-            // date()で日時を出力
-            ;?>月の日記</a><hr>
         </div>
 
         <div id="New" class="tabcontent">
+          <div class="col-xs-9">
+            <div class="write">
+              <form method="POST" action="" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <input type="title" name="input_name" class="form-control" id="title" placeholder="title">
+                    </div>
+                    <div class="form-group">
+                        <input type="content" name="input_content" class="form-control" id="content" placeholder="content">
+                    </div>
+                    <input type="submit" class="btn btn-warning" value="post" >
+                </form>
+            </div>
+          </div>
+        </div>
+
+        <div id="1" class="tabcontent">
           <div class="col-xs-9">
             <div class="content">
               <h3>こんにちわ</h3>
@@ -52,7 +59,7 @@ $time = intval(date('H'));
           </div>
         </div>
 
-        <div id="Pre" class="tabcontent">
+        <div id="2" class="tabcontent">
           <div class="col-xs-9">
             <div class="content">
               <h3>こんにちわ</h3>
@@ -60,24 +67,11 @@ $time = intval(date('H'));
             </div>
           </div>
         </div>
-
-        <div id="2mon" class="tabcontent">
-          <div class="col-xs-9">
-            <div class="content">
-              <h3>こんにちわ</h3>
-              <p>11月10日</p><br>
-            </div>
-          </div>
-
-        </div>
-
-
       </div>
     </div>
     <?php include('layouts/footer.php'); ?>
   </div>
 </body>
-
 <script>
 
 function openCity(evt, cityName) {
@@ -96,6 +90,6 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-</script>
 
+</script>
 </html>
